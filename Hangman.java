a318e3d7b9478aaf70cd426435295751df56e427
S <s>
2017-02-07 22:03:27 -0800

    {"types":"C","runs":313}

--- Hangman.java
+++ Hangman.java
@@ -49,7 +49,6 @@ public class Hangman extends ConsoleProgram {
 	 ***********************************************************/
 
 	public void run() {
-		List<String> list = new ArrayList<String>();
 	}
 	
 	/**
