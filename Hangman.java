39024c05348cb7b90112a7431b36adfa1908996e
S <s>
2017-02-17 01:30:02 -0800

    {"types":"C","runs":58}

--- Hangman.java
+++ Hangman.java
@@ -179,8 +179,9 @@ public class Hangman extends ConsoleProgram {
 		// rearranging the order into the right one in which they'll break,
 		// so that the rightmost line breaks first, then the leftmost one, etc.
 		for (int i = 0; i <= N_GUESSES / 2; i ++){
-			linesBreak.add(lines.get(N_GUESSES - 1 - i));// linesBreak(0)
-			linesBreak.add(lines.get(i));// linesBreak(1)
+			linesBreak.add(lines.get(N_GUESSES - 1 - i));// linesBreak(0) = lines(6)
+			linesBreak.add(lines.get(i));// linesBreak(1) = lines(0)
+			// etc....
 		}
 		
 		
